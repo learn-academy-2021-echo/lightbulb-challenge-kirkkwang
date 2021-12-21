5da18aa30f75bbbdd45276c2b968a07f82040aad
kirkkwang <k3wang@gmail.com>
2021-12-21 11:48:40 -0800

    added a few more comments

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Square from "./components/Square";
 import LightSwitch from "./components/LightSwitch";
 import "./App.css";
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <>
         <h1 className="head">Flip the Light Switch On and Off</h1>
+        {/* used the new LightSwitch component instead of original Square component */}
         <LightSwitch />
       </>
     );
   }
 }
-export default App;
